e1ef15a692b11ac30497a40e0c2cf36ae8d49b82
StarDust130 <csyadav0513@gmail.com>
2026-04-24 17:25:53 +0530

    feat: enhance logging middleware for incoming requests

--- v1-basics/src/app.ts
+++ v1-basics/src/app.ts
@@ -8,8 +8,20 @@ const app = express();
 // Core middleware
 app.use(express.json());
 
-// 🔥 Custom middleware (IMPORTANT: before routes)
+//! 🔥 Custom middleware (IMPORTANT: before routes)
 app.use(requestIdMiddleware);
+
+//---------------------------
+// 📝 Log every incoming request (method + URL + requestId)
+app.use((req, res, next) => {
+  console.log(
+    `🤓 [${req.headers["x-request-id"]}] ${req.method} ${req.url}`
+  );
+  next();
+});
+
+
+//----------------------------
 
 //! Routes 📍
 app.use("/api/v1/stores", storeRoutes );
