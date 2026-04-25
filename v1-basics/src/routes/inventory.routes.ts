fd52d0c617ae1aa981a81a38dbb8e8a74534b147
StarDust130 <csyadav0513@gmail.com>
2026-04-25 23:12:14 +0530

    feat: update inventory routes to use syncInventory and validate schema

--- v1-basics/src/routes/inventory.routes.ts
+++ v1-basics/src/routes/inventory.routes.ts
@@ -1,12 +1,20 @@
 import { Router } from "express";
-import { getStoreInventory, updateInventory } from "../controllers/inventory.controller";
+import {
+  getStoreInventory,
+  syncInventory,
+} from "../controllers/inventory.controller";
+import { validate } from "../utils/validate";
+import { syncInventorySchema } from "../types/inventory.schema";
 
 
 const router = Router()
 
-router.post("/", updateInventory);
-router.get("/", getStoreInventory);
+// 📍 POST /api/v1/inventory (Uses Zod Shield)
+router.post('/', validate(syncInventorySchema), syncInventory);
+
+// 📍 GET /api/v1/inventory/1234-5678-abcd... (Get items in ONE store)
+router.get('/:storeId', getStoreInventory);
 
 
 
-export default Router
+export default router
