6135528978e1dcc6aae20d83ec7d097dc6c92bc4
StarDust130 <csyadav0513@gmail.com>
2026-04-25 17:56:26 +0530

    feat: add inventory routes and integrate into main application

--- v1-basics/src/app.ts
+++ v1-basics/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import storeRoutes from "./routes/store.routes";
 import productRoutes from "./routes/product.routes"
+import inventorytRoutes from "./routes/inventory.routes";
 
 import { globalErrorHandler } from "./utils/error";
 import { requestIdMiddleware } from "./middleware/requestId";
@@ -27,7 +28,8 @@ app.use((req, res, next) => {
 
 //! Routes 📍
 app.use("/api/v1/stores", storeRoutes );
-app.use("/api/v1/products", productRoutes );
+app.use("/api/v1/products", productRoutes ); 
+app.use("/api/v1/inventory", inventorytRoutes); 
 
 //🐞🐞 --- ERROR HANDLING (Must be at the bottom!) 🐞🐞 ---
 app.use(globalErrorHandler); // ⬅️ The Janitor waits here for any thrown errors
