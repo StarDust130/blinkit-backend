121197e67fc60fb505a50ce2c7498dbedb9357a5
StarDust130 <csyadav0513@gmail.com>
2026-04-25 12:12:44 +0530

    feat: enhance getAllStores endpoint with pagination and improved response structure

--- v1-basics/src/controllers/store.controller.ts
+++ v1-basics/src/controllers/store.controller.ts
@@ -29,27 +29,42 @@ return sendResponse({
 });
 });
 
-//! Get All Stores - GET /api/v1/stores 🚀
+//! Get All Stores - GET /api/v1/stores 🌍
 export const getAllStores = catchAsync(async(req: Request, res: Response) => {
-  // 1️⃣ Get all stores from DB
+  // 1️⃣) 🧮 Pagination Setup (Read from URL, e.g., ?limit=10&page=2)
+  // If user doesn't send page or limit, we default to page 1, limit 10
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  // 🦘 Calculate how many rows to skip
+  const offset = (page - 1) * limit;
+
+  // 2️⃣) 🔍 Fetch from Database
   const result = await pool.query(
-    "SELECT id, name, latitude, longitude  FROM stores",
+    `SELECT id, name, latitude, longitude  
+    FROM stores 
+    ORDER BY created_at DESC -- 🕒 Newest stores first
+    LIMIT $1 OFFSET $2 -- 🛑 Stop the server from crashing!
+
+    `,
+    [limit, offset],
   );
 
   // Exact Stores and Total Stores from results
   const stores = result.rows;
-  const total_stores = result.rowCount;
+  const total_result = result.rowCount;
+  const totalResult = await pool.query("SELECT COUNT(*) FROM stores");
+  const total_stores = Number(totalResult.rows[0].count);
 
-
-
-  //2️⃣ Send all stores to user
+  // 3️⃣) 🎉 Success Response
   return sendResponse({
     req,
     res,
     statusCode: 201,
-    message: "All Stores send succesfully✌️",
+    message: "🌍 Stores fetched successfully",
     data: {
       total_stores,
+      total_result,
       stores,
     },
   });
