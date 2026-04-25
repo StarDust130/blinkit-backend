cb28823a1c6fbfbe769bf06dc935d49562df7f9b
StarDust130 <csyadav0513@gmail.com>
2026-04-25 10:15:43 +0530

    feat: add getAllStores route to retrieve all stores

--- v1-basics/src/routes/store.routes.ts
+++ v1-basics/src/routes/store.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
-import { createStore } from "../controllers/store.controller";
+import { createStore, getAllStores } from "../controllers/store.controller";
 import { validate } from "../utils/validate";
 import { createStoreSchema } from "../types/store.schema";
 
 const router = Router();
 
 // POST /api/v1/stores
+router.get("/", getAllStores);
+
 router.post("/", validate(createStoreSchema), createStore);
+
 
 export default router;
