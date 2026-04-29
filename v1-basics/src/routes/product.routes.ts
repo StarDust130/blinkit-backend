08ee5569d436e0379e0ee319aa23c6f3f245e26e
StarDust130 <csyadav0513@gmail.com>
2026-04-29 09:56:53 +0530

    feat: add validation middleware for product creation endpoint

--- v1-basics/src/routes/product.routes.ts
+++ v1-basics/src/routes/product.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { createProduct, getAllProducts } from "../controllers/product.controller";
+import { validate } from "../utils/validate";
+import { syncProductSchema } from "../types/product.schema";
 
 
 const router = Router()
 
 // /api/v1/createProduct
-router.post("/", createProduct);
+router.post("/", validate(syncProductSchema) , createProduct);
 router.get("/", getAllProducts);
 
 
