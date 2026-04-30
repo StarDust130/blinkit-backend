aa970fdbe309bc58dd6b286d2e0377a7c46d760a
StarDust130 <csyadav0513@gmail.com>
2026-04-30 17:12:34 +0530

    feat: implement createProduct function for product creation endpoint

---
+++ v2-mid/src/types/product.schema.ts
@@ -0,0 +1,28 @@
+import {Request, Response } from "express";
+import { catchAsync } from "../utils/catchAsync";
+import prisma from "../config/prisma";
+import { sendResponse } from "../utils/response";
+
+//! Create Product - POST /api/v2/product
+export const  createProduct =  catchAsync(async (req:Request , res: Response) => {
+  const { name, description, basePrice, category } = req.body;
+
+  //1️⃣)  📦 Create Products in DB (Prisma)
+  const createProduct = await prisma.product.create({
+    data: {
+      name,
+      description,
+      basePrice,
+      category,
+    },
+  });
+
+  // 2️⃣ 🎉 Send success response
+  return sendResponse({
+    req,
+    res,
+    statusCode: 201,
+    message: "📦 Product created successfully",
+    data: { store: createProduct },
+  });
+})
