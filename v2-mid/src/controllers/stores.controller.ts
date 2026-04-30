28a6c2802ddfbd0fa35967f1bbc3e33925f447ca
StarDust130 <csyadav0513@gmail.com>
2026-04-30 12:09:18 +0530

    feat: add createStores controller for store creation

--- v2-mid/src/controllers/stores.controller.ts
+++ v2-mid/src/controllers/stores.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { catchAsync } from "../utils/catchAsync";
 
 
+export const createStores = catchAsync(async (req: Request, res: Response) => {});
+
 export const getAllStores = catchAsync(async (req: Request , res: Response) => {
 
 });
