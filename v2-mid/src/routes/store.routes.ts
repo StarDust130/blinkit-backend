d76adc168e9a79851745342681179255296dc89a
StarDust130 <csyadav0513@gmail.com>
2026-04-30 16:18:50 +0530

    feat: implement validation schema for store creation using Zod

--- v2-mid/src/routes/store.routes.ts
+++ v2-mid/src/routes/store.routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createStore, getAllStores } from "../controllers/store.controller";
+import { validate } from "../utils/validate";
+import { createStoreSchema } from "../types/store.schema";
 
 const router = Router()
 
 // POST /api/v1/stores
-router.get("/", createStore);
+router.post("/", validate(createStoreSchema), createStore);
 
 router.get("/", getAllStores);
 
---
+++ v2-mid/src/types/store.schema.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+export const createStoreSchema = z.object({
+  body: z.object({
+    name: z
+      .string({ error: "Store name is required" })
+      .min(3, "Name too short"),
+    latitude: z.number({ error: "Latitude must be a number" }).min(-90).max(90),
+    longitude: z
+      .number({ error: "Longitude must be a number" })
+      .min(-180)
+      .max(180),
+  }),
+});
