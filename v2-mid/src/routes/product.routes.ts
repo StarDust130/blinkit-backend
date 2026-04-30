f4f1db3e62e92e5f129ea5f4294e32fd41541b89
StarDust130 <csyadav0513@gmail.com>
2026-04-30 17:22:15 +0530

    feat: update product routes to use POST for createProduct and add input validation

--- v2-mid/src/routes/product.routes.ts
+++ v2-mid/src/routes/product.routes.ts
@@ -3,11 +3,13 @@ import { createProduct } from "../controllers/product.controller";
 import { validate } from "../utils/validate";
 import { productBodySchema } from "../types/product.schema";
 
+const router = Router();
 
+// 📦 Create Product
+router.post(
+  "/",
+  validate(productBodySchema), // 🧪 validate input first
+  createProduct, // 🚀 then run controller
+);
 
-const router = Router()
-
-// Create - POST
-router.get("/", validate(productBodySchema)  ,createProduct);
-
-export default router
+export default router;
