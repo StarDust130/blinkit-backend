2190e864dc1d85c84edf4d9e7901e3b17226186b
StarDust130 <csyadav0513@gmail.com>
2026-04-30 16:53:14 +0530

    feat: enable requestIdMiddleware and update logging format for incoming requests

--- v2-mid/src/app.ts
+++ v2-mid/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { globalErrorHandler } from "./utils/error";
 import storeRouters from "./routes/store.routes"
+import { requestIdMiddleware } from "./middleware/requestIdMiddleware";
 
 
 
@@ -10,14 +11,12 @@ const app = express();
 app.use(express.json());
 
 //! 🔥 Custom middleware (IMPORTANT: before routes)
-// app.use(requestIdMiddleware);
+app.use(requestIdMiddleware);
 
 //---------------------------
 // 📝 Log every incoming request (method + URL + requestId)
 app.use((req, res, next) => {
-  console.log(
-    `🤓 [${req.headers["x-request-id"]}] ${req.method} ${req.url}`
-  );
+  console.log(`🤓 [${(req as any).requestId}] ${req.method} ${req.url}`);
   next();
 });
 
