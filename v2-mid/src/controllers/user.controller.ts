1d8b8f65b5a9c542ad144f1b885a8c034929f375
StarDust130 <csyadav0513@gmail.com>
2026-05-01 12:07:29 +0530

    feat: enhance createUser controller with error handling and pre-check for existing users

--- v2-mid/src/controllers/user.controller.ts
+++ v2-mid/src/controllers/user.controller.ts
@@ -1,26 +1,45 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import prisma from "../config/prisma";
 import { catchAsync } from "../utils/catchAsync";
 import { sendResponse } from "../utils/response";
+import { AppError } from "../utils/AppError";
 
 //! Create User - POST /api/v2/user
-export const createUser = catchAsync(async (req: Request, res: Response) => {
-  const { name, email, phone } = req.body;
+export const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { name, email, phone } = req.body;
 
-  //1️⃣) Save the user 😤
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      phone,
-    },
-  });
+    // 1️⃣ The Pre-Check (Takes 2ms. Prevents the DB from throwing a fatal constraint error)
+    const existingUser = await prisma.user.findFirst({
+      where: {
+        OR: [{ email }, { phone }],
+      },
+    });
 
-  //2️⃣) Send Response
-  return sendResponse({
-    res,
-    message: "👤User Created Successfully 🌟✌️",
-    statusCode: 201,
-    data: {user}
-  });
-});
+    // 2️⃣ Surgical Error Throwing
+    if (existingUser) {
+      if (existingUser.email === email) {
+        return next(
+          new AppError(`The email ${email} is already registered.`, 400),
+        );
+      }
+      if (existingUser.phone === phone) {
+        return next(
+          new AppError(`The phone number ${phone} is already registered.`, 400),
+        );
+      }
+    }
+
+    // 3️⃣ Safe Execution
+    const user = await prisma.user.create({
+      data: { name, email, phone },
+    });
+
+    return sendResponse({
+      res,
+      message: "👤 User Created Successfully 🌟✌️",
+      statusCode: 201,
+      data: { user },
+    });
+  },
+);
