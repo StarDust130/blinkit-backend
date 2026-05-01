06a6023ec96501b7f212bad3f2156243fb1e8129
StarDust130 <csyadav0513@gmail.com>
2026-05-01 12:18:38 +0530

    feat: update request logging to use x-request-id header

--- v2-mid/src/app.ts
+++ v2-mid/src/app.ts
@@ -19,10 +19,9 @@ app.use(requestIdMiddleware);
 //---------------------------
 // 📝 Log every incoming request (method + URL + requestId)
 app.use((req, res, next) => {
-  console.log(`🤓 [${(req as any).requestId}] ${req.method} ${req.url}`);
+  console.log(`🤓 [${req.headers["x-request-id"]}] ${req.method} ${req.url}`);
   next();
 });
-
 
 //----------------------------
 
