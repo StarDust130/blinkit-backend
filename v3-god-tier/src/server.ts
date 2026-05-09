9fd3267b8c21701838cadea6d140ba4d41a4fbca
StarDust130 <csyadav0513@gmail.com>
2026-05-09 21:40:04 +0530

    fix: update import statements to include file extensions for consistency

--- v3-god-tier/src/server.ts
+++ v3-god-tier/src/server.ts
@@ -1,6 +1,6 @@
-import app from "./app";
-import { env } from "./config/env";
-import { logger } from "./infrastructure/logger/logger";
+import app from "./app.js";
+import { env } from "./config/env.js";
+import { logger } from "./infrastructure/logger/logger.js";
 
 app.listen(env.PORT, () => {
   logger.info(`Server running on port ${env.PORT}`);
